7a63516105355f715ca12e62a5c1b5c58e1ae925
bhargavbhatiya <bhatiyabhargav.bb@gmail.com>
2020-10-07 13:30:49 +0530

    changed files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:skin_ML/main.dart';
+import 'package:skin_ML/Homepage/HomePage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
