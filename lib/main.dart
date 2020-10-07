b89d333acbcbb506968508226a449f717415c76d
Deep <deeppatel2.2312@gmail.com>
2020-10-07 10:19:04 +0530

    Fixed bot

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:skin_ML/model.dart';
+import 'package:skin_ML/model2.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'Named Routes Demo',
+    // Start the app with the "/" named route. In this case, the app starts
+    // on the FirstScreen widget.
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => MyApp(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => SecondScreen(),
+    },
+  ));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
+        home: ML_Model(),
+        title: 'My ML app',
+      );
+  }
+}
+
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: MyApp2(),
       title: 'My ML app',
-      home: ML_Model(),
     );
   }
 }
+
