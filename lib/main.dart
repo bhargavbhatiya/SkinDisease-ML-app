e9be912f751096352bc549b2189931291fbd51a7
bhargavbhatiya <bhatiyabhargav.bb@gmail.com>
2020-10-16 16:45:29 +0530

    login successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:skin_ML/Homepage/HomePage.dart';
-import 'package:skin_ML/services/disease_predict_model.dart';
-import 'package:skin_ML/services/chatbot.dart';
-import 'package:skin_ML/login.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-      initialRoute: '/home',
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/home': (context) => MyApp(),
-      },
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/home': (context) => MyApp(),
+    },
   ));
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skin_ML/login.dart';
 
+// ignore: camel_case_types
 class signup extends StatefulWidget {
   @override
   _signupState createState() => _signupState();
@@ -9,20 +10,18 @@ class signup extends StatefulWidget {
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
+// ignore: camel_case_types
 class _signupState extends State<signup> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController email = new TextEditingController();
   TextEditingController passwd = new TextEditingController();
   bool _success;
 
   void _register() async {
-    final FirebaseUser user = (await
-    _auth.createUserWithEmailAndPassword(
+    final FirebaseUser user = (await _auth.createUserWithEmailAndPassword(
       email: email.text.trim(),
       password: passwd.text.trim(),
-    )
-    ).user;
+    ))
+        .user;
     if (user != null) {
       setState(() {
         _success = true;
@@ -39,110 +38,129 @@ class _signupState extends State<signup> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromRGBO(14, 49, 80, 1),
-        body:  Container(
-            color: Color.fromRGBO(14, 49, 80, 1),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Image.asset("assets/SkinShine with text.png"),
-                      Container(
-                        child: const Text('Sign up for new account',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(16),
-                        alignment: Alignment.center,
-                      ),
-                      SizedBox(height: 30,),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          style: TextStyle(
-                              color: Colors.black,
-                          ),
-                          controller: email,
-                          decoration: const InputDecoration(labelText: 'Email'),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return "Email id can't be empty";
-                            }
-                            return null;
-                          },
+        body: Container(
+          color: Color.fromRGBO(14, 49, 80, 1),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Image.asset("assets/SkinShine with text.png"),
+                    Container(
+                      child: const Text(
+                        'Sign up for new account',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          obscureText: true,
-                          controller: passwd,
-                          decoration: const InputDecoration(labelText: 'Password'),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return "Password can't be empty";
-                            }
-                            return null;
-                          },
+                      padding: const EdgeInsets.all(16),
+                      alignment: Alignment.center,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: TextStyle(
+                          color: Colors.black,
                         ),
+                        controller: email,
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return "Email id can't be empty";
+                          }
+                          return null;
+                        },
                       ),
-                      SizedBox(height: 30,),
-                      GestureDetector(
-                        onTap:(){ Navigator.pushReplacement(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        obscureText: true,
+                        controller: passwd,
+                        decoration:
+                            const InputDecoration(labelText: 'Password'),
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return "Password can't be empty";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(builder: (context) => login()),
-                        );},
-                        child: Text("Already have account? Login",
-                          style: TextStyle(color: Colors.white),),
+                        );
+                      },
+                      child: Text(
+                        "Already have account? Login",
+                        style: TextStyle(color: Colors.white),
                       ),
-                      SizedBox(height: 20,),
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        alignment: Alignment.center,
-                        child: RaisedButton(
-                          color: Colors.white,
-                          onPressed: ()  {
-                              _register();
-                          },
-                          child: const Text('Sign up',
-                            style: TextStyle(
-                                color: Colors.black,
-                                backgroundColor: Colors.white
-                            ),
-                          ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      alignment: Alignment.center,
+                      child: RaisedButton(
+                        color: Colors.white,
+                        onPressed: () {
+                          _register();
+                        },
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(
+                              color: Colors.black,
+                              backgroundColor: Colors.white),
                         ),
                       ),
-                      SizedBox(height: 20,),
-                      Container(
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Text(
-                          _success == null
-                              ? ''
-                              : (_success
-                              ? Future.delayed(Duration.zero, (){
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => login()),
-                            );},)
-                              : 'Sign in failed'),
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        _success == null
+                            ? ''
+                            : (_success
+                                ? Future.delayed(
+                                    Duration.zero,
+                                    () {
+                                      Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => login()),
+                                      );
+                                    },
+                                  )
+                                : 'Sign in failed'),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
         ),
+      ),
     );
   }
 
